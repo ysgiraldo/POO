ff1a03c651e6114176f2336832fec308feb3aa02
ysgiraldo <ygiraldoca@unal.edu.co>
2024-04-07 11:19:41 -0500

    Update metodos especiales

--- 14_metodos especiales.py
+++ 14_metodos especiales.py
@@ -3,7 +3,13 @@
 '''
 Son funciones que tienen un nombre especial reservado.
 Estan creados con la unica finalidad de que nosotros podamos crear 
-funcionalidades especiales que con metodos especiales no podríamos crear
+funcionalidades especiales que con metodos normales no podríamos crear
+
+los metodos dunder son metodos especiales que nos permiten hacer cosas
+especiales con nuestras clases, como por ejemplo, poder sumar dos objetos
+de una clase que nosotros creamos, o poder imprimir un objeto de una clase
+de una forma especial, o poder comparar dos objetos de una clase de una forma
+especial, etc.
 '''
 
 class Persona:
@@ -12,31 +18,36 @@ class Persona:
         self.edad = edad
 
     # representacion del objeto en una cadena de texto
-    def __str__(self):
+    def __str__(self): # devolver una representacion del objeto en una cadena de texto
         return f'Persona(nombre={self.nombre},edad={self.edad})'
     
-    def __repr__(self):
+    def __repr__(self): # actue como la representacion del objeto en una cadena de texto
         return f'Persona("{self.nombre}",{self.edad})'
     
     #sobrecarga de operadores
-    def __add__(self,otro):
+    def __add__(self,otro): # modificar la forma en la que se va a comportar
         nuevo_valor = self.edad + otro.edad
         return Persona(self.nombre+otro.nombre,nuevo_valor)
     
 dalto = Persona('Sofia',21)
-print(dalto)
+
 repr = repr(dalto)
 resultado = eval(repr)
+
 print(repr)
-print(resultado)
 print(resultado.nombre)
 print(resultado.edad)
 
 dalto = Persona('Sofia',21)
-dalto2 = Persona('Pedro',24)
+pedro = Persona('Pedro',24)
 maria = Persona('Maria',18)
 
-nueva_persona = dalto + dalto2 + maria
+# se puede sumar objetos de una clase
+nueva_persona = dalto + pedro + maria
 print(nueva_persona.nombre)
 print(nueva_persona.edad)
+
+'''
+Esto es una sobrecarga de operadores, es decir, estamos modificando la forma en la que se va a comportar
+'''
 
