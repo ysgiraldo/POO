264e51e375b621397d7b9576e9cfc9b06042bc45
ysgiraldo <ygiraldoca@unal.edu.co>
2024-04-07 14:53:00 -0500

    Update DIP

--- 15_SOLID/5_DIP.py
+++ 15_SOLID/5_DIP.py
@@ -6,6 +6,13 @@ las abstracciones no deben depender de los detalles sino que los detalles
 deben depender de las abastracciones, en otras palabras, significa que no debemos
 depender de implementaciones especificas o sea de una funcion particular o de algun
 codigo que haga algo sino que tenemos que depender de interfacez mas complejas de cosas mas grandes
+
+RECORDAR:
+
+Modulos de alto nivel son aquellos que proveen la logica de negocio
+Modulos de bajo nivel son aquellos que proveen detalles de implementacion
+Las abstracciones son las interfaces que permiten que los modulos de alto nivel no dependan de los de bajo nivel
+Los detalles son las implementaciones concretas de las abstracciones
 '''
 
 # class Diccionario:
@@ -35,9 +42,9 @@ class Diccionario(VerificadorOrtografico):
         # logica para verificar palabra si esta en el diccionario
         pass
 
-# class ServicioOnline(VerificadorOrtografico):
+# class ServicioWeb(VerificadorOrtografico):
 #     def verificar_palabra(self,palabra):
-#         #logica para verificar palabra si esta en el diccionario
+#         #logica para verificar palabra desde el servicio web
 #         pass
 
 class CorrectorOrtografico:    
